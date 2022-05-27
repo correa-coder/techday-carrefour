169d266b8efea04172cde73735a81c5473b80cbd
correa-coder <guilhermecorrea903723@gmail.com>
2022-05-27 09:47:48 -0300

    create ProductCard component

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,14 @@
+export function ProductCard(props) {
+    return (
+        <div className="card">
+            <a href={props.productObject.link} rel="external" target="_blank">
+                <img src={props.productObject.image} alt={props.productObject.productName} className="img-fluid" />
+                <h3 className="product-title">{props.productObject.productName}</h3>
+                <div className="product-price">
+                    R$ {props.productObject.price}
+                </div>
+            </a>
+            <button className="btn btn-primary">visualizar</button>
+        </div>
+    )
+}
