6a2cf8801f4009880d970d880657497691c4149d
correa-coder <guilhermecorrea903723@gmail.com>
2022-05-27 11:19:19 -0300

    add brand and description to product mock

--- src/utils/mock_data.js
+++ src/utils/mock_data.js
@@ -3,20 +3,26 @@
 export default [
     {
         productName: "Batata Monalisa Aprox. 1 Kg",
+        brand: "Carrefour",
         link: "https://mercado.carrefour.com.br/batata-monalisa-aprox--1-kg-46922/p",
         image: "https://carrefourbr.vteximg.com.br/arquivos/ids/14648737/batata-monalisa-carrefour-600-g-1.jpg?v=637511892564730000",
+        description: "Toda a linha de hortifruti sempre fresquinhos, saudáveis e com qualidade garantida de origem.",
         price: 7.99
     },
     {
         productName: "Alho a granel Aprox. 200g",
+        brand: "Dai Alimentos",
         link: "https://mercado.carrefour.com.br/alho-a-granel-aprox--200g-176869/p",
         image: "https://carrefourbr.vteximg.com.br/arquivos/ids/14648703/alho-granel-kg-1.jpg?v=637511867118130000",
+        description: "Alho Dai Alimentos Kg",
         price: 20.59
     },
     {
         productName: "Óleo de Soja Liza 900ml",
+        brand: "Liza",
         link: "https://mercado.carrefour.com.br/oleo-de-soja-liza-900ml-406830/p",
         image: "https://carrefourbr.vteximg.com.br/arquivos/ids/15891465/oleo-de-soja-liza-900ml-1.jpg?v=637541734580770000",
+        description: "O Liza Soja é um óleo 100% vegetal, cinco vezes extrafiltrado, feito a partir do grão da soja. Por ser óleo de soja, o produto é fonte de Vitamina E e uma ótima opção para quem quer sabor em suas receitas sem descuidar do bem-estar de toda a família, pois os óleos vegetais possuem gorduras que contribuem com uma alimentação saudável, dentro da porção adequada de consumo. Ele não possui gorduras trans e nem glúten. A história da marca Liza começou em 1975, lançando o primeiro óleo de cozinha que não fazia fumaça e nem deixava cheiro. Hoje em dia, além do tradicional óleo, também produz maioneses e molhos para salada. Aproveite a oportunidade e compre seu Óleo de Soja Liza aqui no Carrefour.",
         price: 9.99
     }
 ]
