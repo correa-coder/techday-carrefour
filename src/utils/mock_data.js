10ce55929cdd44fa0eb494b7d5436af080827c9d
correa-coder <guilhermecorrea903723@gmail.com>
2022-05-27 09:46:10 -0300

    create a mock file for products

---
+++ src/utils/mock_data.js
@@ -0,0 +1,22 @@
+// previously fetched data from the API
+
+export default [
+    {
+        productName: "Batata Monalisa Aprox. 1 Kg",
+        link: "https://mercado.carrefour.com.br/batata-monalisa-aprox--1-kg-46922/p",
+        image: "https://carrefourbr.vteximg.com.br/arquivos/ids/14648737/batata-monalisa-carrefour-600-g-1.jpg?v=637511892564730000",
+        price: 7.99
+    },
+    {
+        productName: "Alho a granel Aprox. 200g",
+        link: "https://mercado.carrefour.com.br/alho-a-granel-aprox--200g-176869/p",
+        image: "https://carrefourbr.vteximg.com.br/arquivos/ids/14648703/alho-granel-kg-1.jpg?v=637511867118130000",
+        price: 20.59
+    },
+    {
+        productName: "Óleo de Soja Liza 900ml",
+        link: "https://mercado.carrefour.com.br/oleo-de-soja-liza-900ml-406830/p",
+        image: "https://carrefourbr.vteximg.com.br/arquivos/ids/15891465/oleo-de-soja-liza-900ml-1.jpg?v=637541734580770000",
+        price: 9.99
+    }
+]
